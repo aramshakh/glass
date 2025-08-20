cafc70f679b0198d6b7dc76883bc37fe4729f89e
Aram Shakhbandaryan <140006457+aramshakh@users.noreply.github.com>
2025-08-20 21:29:47 +0400

    feat: detect emotions using NVC feelings

---
+++ src/features/listen/summary/nvcEmotions.js
@@ -0,0 +1,25 @@
+const nvcEmotions = [
+  'affectionate','compassionate','friendly','loving','open hearted','sympathetic','tender','warm',
+  'engaged','absorbed','alert','curious','engrossed','fascinated','interested','involved','intrigued','stimulated',
+  'hopeful','expectant','encouraged','optimistic',
+  'confident','empowered','open','proud','safe','secure',
+  'excited','amazed','ardent','eager','energetic','enthusiastic','exhilarated','inspired','lively','passionate','surprised',
+  'grateful','appreciative','thankful',
+  'joyful','amused','delighted','glad','happy','jubilant','pleased','satisfied','tickled',
+  'peaceful','calm','comfortable','fulfilled','mellow','quiet','relaxed','relieved','serene','tranquil',
+  'refreshed','enlivened','rejuvenated','renewed','rested','restored','revived',
+  'afraid','apprehensive','dread','frightened','mistrustful','panicked','petrified','scared','suspicious','terrified','worried',
+  'angry','enraged','furious','incensed','indignant','irate','livid','outraged','resentful',
+  'annoyed','aggravated','dismayed','exasperated','frustrated','impatient','irritated',
+  'aversion','averse','disgusted','disliking','horrified','nauseated','repulsed',
+  'confused','ambivalent','bewildered','dazed','hesitant','lost','puzzled','torn',
+  'disconnected','aloof','apathetic','bored','cold','detached','distant','indifferent','withdrawn',
+  'disquiet','agitated','alarmed','disturbed','nervous','restless','shocked','startled','tense','troubled','upset',
+  'embarrassed','ashamed','chagrined','flustered','guilty','self-conscious',
+  'exhausted','beat','depleted','fatigued','lethargic','sleepy','tired','weary','worn out',
+  'grief','depressed','despairing','disappointed','discouraged','hopeless','sad','sorrowful','tearful',
+  'pain','anguished','heartbroken','hurt','lonely','miserable','regretful','remorseful',
+  'tense','anxious','cranky','edgy','fidgety','frazzled','jittery','nervous','overwhelmed','stressed'
+];
+
+module.exports = nvcEmotions;
