f660ff4f18932d3a30ec86b3fb626fc983385706
Aram Shakhbandaryan <140006457+aramshakh@users.noreply.github.com>
2025-08-20 21:29:41 +0400

    Add NVC emotion lists

---
+++ src/features/listen/empathy/nvcEmotions.js
@@ -0,0 +1,36 @@
+/**
+ * Emotion categories derived from the Nonviolent Communication (NVC) feelings inventory.
+ * All emotion names are provided in UPPERCASE.
+ * @type {{SATISFIED: string[], NOT_SATISFIED: string[]}}
+ */
+module.exports = {
+    SATISFIED: [
+        'AFFECTIONATE',
+        'ENGAGED',
+        'HOPEFUL',
+        'CONFIDENT',
+        'EXCITED',
+        'GRATEFUL',
+        'INSPIRED',
+        'JOYFUL',
+        'EXHILARATED',
+        'PEACEFUL',
+        'REFRESHED'
+    ],
+    NOT_SATISFIED: [
+        'AFRAID',
+        'AVERSION',
+        'CONFUSED',
+        'ANNOYED',
+        'DISCONNECTED',
+        'ANGRY',
+        'VULNERABLE',
+        'YEARNING',
+        'DISQUIET',
+        'EMBARRASSED',
+        'FATIGUE',
+        'PAIN',
+        'SAD',
+        'TENSE'
+    ]
+};
